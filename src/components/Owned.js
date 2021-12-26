2426dec694c873d7d477b1e31dd0a6ed0e9bc2c2
Nymph <QuantNymph@gmail.com>
2021-12-25 20:15:20 -0600

    removed /ipfs/ from link because its in uri

--- src/components/Owned.js
+++ src/components/Owned.js
@@ -64,7 +64,7 @@ function Owned({connection}) {
       {!isLoading && <div id="gallery">
         {ownedBots.map(b => (
           <div className="arbibot" key={b.tokenId}>            
-            <img src={"https://ipfs.io/ipfs" + b.image} key={"https://ipfs.io/ipfs" + b.image}></img>
+            <img src={"https://ipfs.io" + b.image} key={"https://ipfs.io" + b.image}></img>
             <p>Feet Pic #{b.tokenId}</p>
           </div>
         ))}
