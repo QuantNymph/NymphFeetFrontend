fa79f12c0bbeaee4e9ee32e7b3a8ae83c7255f92
Nymph <QuantNymph@gmail.com>
2022-01-19 01:44:24 -0600

    idk man

--- src/components/Owned.js
+++ src/components/Owned.js
@@ -61,8 +61,9 @@ function Owned({connection}) {
       <br />
 
       {isLoading && <p>Loading owned feet pics... <br /> (rendering might take a while depending on your RPC)</p>}
-      
-      {ownedBots.length > 0 && !isLoading  && <div id="invite"><a href='https://discord.gg/AG5zAhBUCT'>Come to the discord!</a> (verify with collab.land for access)</div> && <div id="gallery">        
+      {!isLoading && ownedBots.length === 0 && <p>You don't own any feet pics yet!</p>}
+      {!isLoading && ownedBots.length > 0 && <div id="invite"><a href='https://discord.gg/AG5zAhBUCT'>Come to the discord!</a> (verify with collab.land for access)</div>}
+      {!isLoading && ownedBots.length > 0 && <div id="gallery">        
         {ownedBots.map(b => (
           <div className="arbibot" key={b.tokenId}>            
             <img width={256} height={'auto'} src={"https://ipfs.io".concat(b.image)} key={"https://ipfs.io".concat(b.image)}></img>
