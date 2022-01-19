0a132d81a323629f640f6187e54e7b8ef8b4c5f8
Nymph <QuantNymph@gmail.com>
2022-01-19 01:33:01 -0600

    i hate web dev

--- src/components/Owned.js
+++ src/components/Owned.js
@@ -62,8 +62,7 @@ function Owned({connection}) {
 
       {isLoading && <p>Loading owned feet pics... <br /> (rendering might take a while depending on your RPC)</p>}
       
-      {ownedBots.length > 0 && !isLoading && <div id="gallery">
-        <div id="invite"><a href='https://discord.gg/AG5zAhBUCT'>Come to the discord!</a> (verify with collab.land for access)</div>
+      {ownedBots.length > 0 && !isLoading  && <div id="invite"><a href='https://discord.gg/AG5zAhBUCT'>Come to the discord!</a> (verify with collab.land for access)</div> && <div id="gallery">        
         {ownedBots.map(b => (
           <div className="arbibot" key={b.tokenId}>            
             <img src={"https://ipfs.io".concat(b.image)} key={"https://ipfs.io".concat(b.image)}></img>
